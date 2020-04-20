196115f7013acf773c8229120d8855f8de0aea05
ryabukhind <dryabuhin@mmtr.ru>
2020-04-20 18:40:20 +0300

    Create project

---
+++ src/Plugins/Nop.Plugin.Widgets.TestWidget/Models/PublicInfoModel.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Web.Framework.Models;
+
+namespace Nop.Plugin.Widgets.TestWidget.Models
+{
+    public class PublicInfoModel : BaseNopModel
+    {
+        public string Text { get; set; }
+    }
+}
